b6ae004745b50eba73b8b1f2a4fc3fe3a72daca4
jlaplante333 <jonathan.laplante@gmail.com>
2025-08-23 03:09:03 +0900

    Switch to HashRouter for static hosting compatibility

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { LanguageProvider } from './context/LanguageContext';
 import { ThemeProvider } from './context/ThemeContext';
 import ProtectedRoute from './components/ProtectedRoute';
